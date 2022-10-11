e12850bea20e535c54091377133aaca26fdf8a27
cartalla26 <114981196+cartalla26@users.noreply.github.com>
2022-10-11 17:56:27 +0800

    initial

---
+++ sendmail.php
@@ -0,0 +1,6 @@
+<?php
+
+    $site_owners_email = 'cartallajmarie@gmail.com'; // Replace this with your own email address
+    $site_owners_name = 'J.MARIE CARTALLA'; // replace with your name
+
+?>
